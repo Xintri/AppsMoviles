3b15c7cdcae37b5503ad368eff0de0e4fed9c335
rito <muges453012@gmail.com>
2024-10-23 20:37:25 -0600

    clase de hoy (23/10/2024)

--- UNIDAD2_071024/clase_231024/script.js
+++ UNIDAD2_071024/clase_231024/script.js
@@ -1,18 +1,15 @@
 let task = ["Comprar", "Edtudiar", "Limpiar"];
 
 console.log("Lista de tareas: ", task),
-
 task.push("Ir a la escuela");
-
 task.splice(0,2);
-
 task.push("Ir a casa");
 
 
 //___________________Usando ForEach_________________________//
 
-let Count = ["Comprar", "Edtudiar", "Limpiar"];
+let CONTADEISHON = ["Regateando en el mercado", "Estudihambre", "Señora ama de casa"];
 
-Count.forEach((task, index) => {
-    console.log("Tarea " + index + ": " + task);
+CONTADEISHON.forEach((task, index) => {
+    console.log("Para hacer " + (index + 1 ) + ": " + task);
 });
